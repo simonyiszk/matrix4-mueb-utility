399bb7ceed7a42323a28556b76ea0e66d5c1ad00
Omcsesz <omcsesz@gmail.com>
2018-05-30 22:13:30 +0200

    New commands added, values changed

--- udpTest/UDP.cs
+++ udpTest/UDP.cs
@@ -103,23 +103,35 @@ namespace udpTest
             byte commandCode= 0;
 
             switch (commandToCast) {
-                case "12V-off":
+                case "12V-off-left":
                     commandCode = 0;
                     break;
-                case "12V-on":
+                case "12V-off-right":
                     commandCode = 1;
                     break;
-                case "reboot":
+                case "reset-left-panel":
                     commandCode = 2;
                     break;
-                case "get-status":
+                case "reset-right-panel":
                     commandCode = 3;
                     break;
-                case "start-animation":
+                case "reboot":
                     commandCode = 4;
                     break;
-                case "stop-animation":
+                case "get-status":
                     commandCode = 5;
+                    break;
+                case "use-internal-animation":
+                    commandCode = 10;
+                    break;
+                case "use-external-animation":
+                    commandCode = 20;
+                    break;
+                case "blank":
+                    commandCode = 30;
+                    break;
+                case "delete-anim-network-buffer":
+                    commandCode = 6;
                     break;
                 default:
                     break;
